b842485949c5e5e3defe6db910b8ed1d9a98e2e8
neelrshah <neelrshah1241@gmail.com>
2022-04-11 17:00:11 +0530

    middle ware for testbackend added

--- lcomernbootcamp/testback/index.js
+++ lcomernbootcamp/testback/index.js
@@ -1,24 +1,38 @@
 const express = require("express");
+
 const app = express();
-//const port = 3000
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
-
 
 const port = 8000;
-//create routes
-app.get("/login", (req, res)=>{
-    return res.send("You are visting login route");
-});
-app.get("/signout", (req, res)=>{
-    return res.send("You are signed out");
+
+app.get("/", (req, res) => {
+  return res.send("Home page");
 });
 
-app.listen(port,()=>{
-    console.log("Server is up and running");
+const admin = (req, res) => {
+  return res.send("this is admin dashboard");
+};
+
+const isAdmin = (req, res, next) => {
+  console.log("isAdmin is running");
+  next();
+};
+
+app.get("/admin", isAdmin, admin);
+
+app.get("/login", (req, res) => {
+  return res.send("You are visiting login route");
 });
+
+app.get("/signup", (req, res) => {
+  return res.send("You are visiting signup route");
+});
+
+app.listen(port, () => {
+  console.log("Server is up and running...");
+});
+
+// const port = 3000
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
