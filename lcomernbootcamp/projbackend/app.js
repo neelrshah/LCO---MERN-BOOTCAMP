1548c6bc38d9f42621f23adaae166326652295ad
neelrshah <neelrshah1241@gmail.com>
2022-04-11 17:29:18 +0530

    router added for projbackend

--- lcomernbootcamp/projbackend/app.js
+++ lcomernbootcamp/projbackend/app.js
@@ -7,6 +7,9 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+const authRoutes = require("./routes/auth");
+
+//DB Connection
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -17,13 +20,18 @@ mongoose
     console.log("DB CONNECTED");
   });
 
-  //Middlewares
+//Middlewares
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
+//My Routes
+app.use("/api", authRoutes);
+
+//PORT
 const port = process.env.PORT || 8000;
 
+//Starting a server
 app.listen(port, () => {
   console.log(`app is running at ${port}`);
 });
